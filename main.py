3f74b01f852a8a97e27ef4be4b913b180aa3edc0
pooriya <adibradp@gmail.com>
2024-07-02 16:51:23 +0330

    my main windows be fixed

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+windows=Tk()
+windows.geometry('800x600')
+
+
+lblWelcome=Label(windows,text='Welcome to DevOps world !')
+lblWelcome.pack(side='top',fill='both')
+windows.mainloop()
